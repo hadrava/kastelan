bc7fe08fb59ede3c9b234f040e96b4ae81a965a4
Jan Hadrava <had@kam.mff.cuni.cz>
2014-03-11 14:34:10 +0100

    smazání nepoužitelných věcí

--- garbage/make/build_test_c328.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Load config
-. ../config
-prepare_build_arm
-
-bfin-uclinux-g++ -I src/common/ src/common/c328.cpp -o build_arm/c328
-bfin-uclinux-g++ -I src/common/ src/test/c328_read.cpp -o build_arm/c328_read
--- garbage/make/build_x86_c328_image_reader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Load config
-. ../config
-prepare_build_x86
-
-g++ -lopencv_core -lopencv_highgui -lopencv_imgproc src/test/c328_image_reader/viewer.cpp -o build_x86/c328_image_reader
