fdb3c13d97614b39d631f6a1e3d157c48094a53f
Jan Hadrava (pi@kastelan) <had+kastelan@kam.mff.cuni.cz>
2014-03-23 12:01:26 +0100

    src: atmega_enkodery: build+flash je kompatibilni s makefile

--- src/atmega_enkodery/Dekoder/build.sh
+++ src/atmega_enkodery/Dekoder/build.sh
@@ -1,6 +1,6 @@
 avr-gcc -Wall -std=gnu99 -Os -finline-functions -I. -DDEBUG_LEVEL=0 -mmcu=atmega48p -c decoder.c -o decoder.o
 avr-gcc -Wall -std=gnu99 -Os -finline-functions -I. -DDEBUG_LEVEL=0 -mmcu=atmega48p -c TWI_slave.c -o TWI_slave.o
-avr-gcc -Wall -std=gnu99 -Os -finline-functions -I. -DDEBUG_LEVEL=0 -mmcu=atmega48p -o main.elf decoder.o TWI_slave.o
-rm -f main.hex main.eep.hex
-avr-objcopy -j .text -j .data -O ihex main.elf main.hex
-avr-size main.hex
+avr-gcc -Wall -std=gnu99 -Os -finline-functions -I. -DDEBUG_LEVEL=0 -mmcu=atmega48p -o decoder.elf decoder.o TWI_slave.o
+rm -f decoder.hex decoder.eep.hex
+avr-objcopy -j .text -j .data -O ihex decoder.elf decoder.hex
+avr-size decoder.hex
--- src/atmega_enkodery/Dekoder/flash.sh
+++ src/atmega_enkodery/Dekoder/flash.sh
@@ -1,5 +1,5 @@
 avr-size main.hex
-avrdude -p m48p -c usbasp -U flash:w:main.hex -v
+avrdude -p m48p -c usbasp -U flash:w:decoder.hex -v
 
 #clk 8MHz
 #avrdude -p m8 -c usbasp -U lfuse:w:0xe4:m -v
