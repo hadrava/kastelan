d260471cfe5a3aa247fe269427d70f9a8422d531
Jan Hadrava (pi@kastelan) <had+kastelan@kam.mff.cuni.cz>
2014-03-23 12:57:40 +0100

    atmega_enkodery: funkcni hex + uklid

--- src/atmega_enkodery/Dekoder/build.sh
+++
@@ -1,6 +0,0 @@
-avr-gcc -Wall -std=gnu99 -Os -finline-functions -I. -DDEBUG_LEVEL=0 -mmcu=atmega48p -c decoder.c -o decoder.o
-avr-gcc -Wall -std=gnu99 -Os -finline-functions -I. -DDEBUG_LEVEL=0 -mmcu=atmega48p -c TWI_slave.c -o TWI_slave.o
-avr-gcc -Wall -std=gnu99 -Os -finline-functions -I. -DDEBUG_LEVEL=0 -mmcu=atmega48p -o decoder.elf decoder.o TWI_slave.o
-rm -f decoder.hex decoder.eep.hex
-avr-objcopy -j .text -j .data -O ihex decoder.elf decoder.hex
-avr-size decoder.hex
