7dd71a9cb73530b00c2730a4f2dd8d8e1c1daad5
Jan Hadrava <had@kam.mff.cuni.cz>
2014-04-23 17:31:09 +0200

    gui: batch_convert trochu paralelizuje, mění barvy a vysekává obrázky se skokem

--- src/gui/batch_convert.sh
+++ src/gui/batch_convert.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 
-list="`find logs/g1/ logs/final/ -type f`"
+convert() {
+	i="$1"
+	shift
 
-make gui_image
-for i in $list; do
 	mkdir -p tmp/$i
 	pwd="`pwd`"
 	cd tmp/$i
-	"${pwd}/gui_image" < "${pwd}/$i"
-	cd "$pwd"
-done
-
-mkdir -p out
-for i in $list; do
-	pwd="`pwd`"
-	cd tmp/$i
+	"${pwd}/gui_image" "$@" < "${pwd}/$i" > gui_image.log
 	"${pwd}/convert_to_video.sh"
+
 	cd "$pwd"
 	j=`echo "$i" | tr "/" "_"`
-	mv tmp/${i}/out.mkv out/${j}.mkv
+	# Copy jump-images
+	mkdir -p out/$j
+	tail -n+2 tmp/${i}/gui_image.log | cat -n | grep 'speed.*[0-9][0-9][0-9][0-9]' | sed 's/speed.*/- 1 + 10000000000/' | while read line; do expr $line;done | sed 's#^1\(.*\)#cp tmp/'${i}'/image-\1.png out/'$j'/#' | sh
+
+	cp tmp/${i}/out.mkv out/${j}.mkv
+	cp tmp/${i}/gui_image.log out/${j}.log
+}
+
+# Generate list:
+# list="`find logs/g1/ logs/final/ -type f`"
+
+listR="logs/g1/prvni_zapas_polaci logs/g1/treti logs/g1/paty logs/final/4_rus logs/final/5_ten_cerny_z_g2 logs/final/6_rakusan"
+listB="logs/g1/druhy_zapas_angstrom logs/g1/ctvrty logs/g1/sesty logs/final/1 logs/final/2_rur logs/final/3_jacek logs/final/7_chees"
+
+# Debug:
+#listR=""
+#listB="logs/final/7_chees"
+
+make gui_image
+rm -rf old_tmp/
+rm -rf old_out/
+mv tmp old_tmp
+mv out old_out
+mkdir -p out
+
+for i in $listR; do
+	convert $i -c R&
 done
+wait
+
+for i in $listB; do
+	convert $i -c B&
+done
+wait
